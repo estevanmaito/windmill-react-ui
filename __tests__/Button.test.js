33390889ccee74a4ff7518ad15832fa4cea55d4c
Estevan Maito <ejmaito@gmail.com>
2020-06-23 12:28:45 -0300

    test(dropdownitem): add dropdownItem button test

--- __tests__/Button.test.js
+++ __tests__/Button.test.js
@@ -193,3 +193,13 @@ describe('Link Button', () => {
     expect(wrapper.find('button[disabled]').getDOMNode().getAttribute('class')).toContain(expected)
   })
 })
+
+describe('DropdowItem Button', () => {
+  it('should contain DropdowItem button classes', () => {
+    const expected =
+      'inline-flex items-center cursor-pointer w-full px-2 py-1 text-sm font-medium transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200'
+    const wrapper = mount(<Button layout="__dropdownItem" />)
+
+    expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
+  })
+})
