e6db66da36af21c825c0491ff37466910040e623
Estevan Maito <ejmaito@gmail.com>
2020-06-23 18:03:02 -0300

    chore(test): fix typo

--- __tests__/Button.test.js
+++ __tests__/Button.test.js
@@ -194,7 +194,7 @@ describe('Link Button', () => {
   })
 })
 
-describe('DropdowItem Button', () => {
+describe('DropdownItem Button', () => {
   it('should contain DropdowItem button classes', () => {
     const expected =
       'inline-flex items-center cursor-pointer w-full px-2 py-1 text-sm font-medium transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200'
