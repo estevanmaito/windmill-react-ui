99830107e66706f583f8552ef1d63a8b09afd337
Estevan Maito <ejmaito@gmail.com>
2020-06-25 15:23:10 -0300

    fix(windmill): export windmill

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+export { default as Windmill } from './Windmill'
 export { default as Button } from './Button'
 export { default as Card } from './Card'
 export { default as CardBody } from './CardBody'
