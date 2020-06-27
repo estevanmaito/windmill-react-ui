980d4439a7a47d533cc4a344d55456fbdbb23387
Estevan Maito <ejmaito@gmail.com>
2020-06-27 14:45:34 -0300

    build(webpack): add globalObject this
    fix the window is not defined when SSR

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
+    globalObject: 'this',
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     library: 'windmill-react-ui',
