48c4348e6503296ddddabe5a44211f9302d88826
Estevan Maito <ejmaito@gmail.com>
2020-07-11 22:05:12 -0300

    fix(backdrop): use z index 40
    z-50 wouldn't leave space for the user to add elements over the backdrop

--- __tests__/Backdrop.test.js
+++ __tests__/Backdrop.test.js
@@ -9,7 +9,7 @@ describe('Backdrop', () => {
 
   it('should render with base styles', () => {
     const expected =
-      'fixed inset-0 z-50 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center'
+      'fixed inset-0 z-40 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center'
     const wrapper = mount(<Backdrop />)
 
     expect(wrapper.find(Backdrop).getDOMNode().getAttribute('class')).toContain(expected)
