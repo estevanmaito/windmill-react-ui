6657d52a03e84b8805d8ea7ea71af719694a7d45
Estevan Maito <ejmaito@gmail.com>
2020-07-19 13:48:15 -0300

    fix(backdrop): add forwardRef to backdrop
    BREAKING CHANGE: Backdrop now supports forwarding refs.

--- src/Backdrop.js
+++ src/Backdrop.js
@@ -3,7 +3,7 @@ import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import { ThemeContext } from './context/ThemeContext'
 
-function Backdrop(props) {
+const Backdrop = React.forwardRef(function Backdrop(props, ref) {
   const { className, ...other } = props
   const {
     theme: { backdrop },
@@ -12,8 +12,8 @@ function Backdrop(props) {
   const baseStyle = backdrop.base
 
   const cls = classNames(baseStyle, className)
-  return <div className={cls} {...other}></div>
-}
+  return <div className={cls} ref={ref} {...other}></div>
+})
 
 Backdrop.propTypes = {
   className: PropTypes.string,
