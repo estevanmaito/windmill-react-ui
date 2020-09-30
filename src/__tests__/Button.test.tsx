9caf06c272dd69f7cb9b5682a1335b6e19ecb9ac
HerbCaudill <herb@devresults.com>
2020-09-30 12:05:24 +0200

    Button.test.tsx: add aria-label to quiet warnings

--- src/__tests__/Button.test.tsx
+++ src/__tests__/Button.test.tsx
@@ -5,41 +5,41 @@ import HeartIcon from './utils/heart.svg'
 
 describe('Base Button', () => {
   it('should render without crashing', () => {
-    mount(<Button />)
+    mount(<Button aria-label="test" />)
   })
 
   it('should render a button element', () => {
-    const wrapper = mount(<Button />)
+    const wrapper = mount(<Button aria-label="test" />)
 
     expect(wrapper.find('button')).toHaveLength(1)
   })
 
   it('should render a button with type button', () => {
-    const wrapper = mount(<Button />)
+    const wrapper = mount(<Button aria-label="test" />)
 
     expect(wrapper.find('button[type="button"]')).toHaveLength(1)
   })
 
   it('should render a button with type submit', () => {
-    const wrapper = mount(<Button type="submit" />)
+    const wrapper = mount(<Button aria-label="test" type="submit" />)
 
     expect(wrapper.find('button[type="submit"]')).toHaveLength(1)
   })
 
   it('should render a button with type reset', () => {
-    const wrapper = mount(<Button type="reset" />)
+    const wrapper = mount(<Button aria-label="test" type="reset" />)
 
     expect(wrapper.find('button[type="reset"]')).toHaveLength(1)
   })
 
   it('should render an anchor element', () => {
-    const wrapper = mount(<Button tag="a" />)
+    const wrapper = mount(<Button aria-label="test" tag="a" />)
 
     expect(wrapper.find('a')).toHaveLength(1)
   })
 
   it('should not contain type for anchor element', () => {
-    const wrapper = mount(<Button tag="a" />)
+    const wrapper = mount(<Button aria-label="test" tag="a" />)
 
     expect(wrapper.find('a').getDOMNode().getAttribute('type')).toBeNull()
   })
@@ -53,39 +53,39 @@ describe('Base Button', () => {
   it('should contain base classes', () => {
     const expected =
       'inline-flex items-center justify-center cursor-pointer leading-5 transition-colors duration-150 font-medium focus:outline-none'
-    const wrapper = mount(<Button />)
+    const wrapper = mount(<Button aria-label="test" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
 
   it('should call onClick callback', (done) => {
-    mount(<Button onClick={() => done()} />).simulate('click')
+    mount(<Button aria-label="test" onClick={() => done()} />).simulate('click')
   })
 
   it('should contain the appropriate type', () => {
     const expected = 'submit'
-    const wrapper = mount(<Button type="submit" />)
+    const wrapper = mount(<Button aria-label="test" type="submit" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('type')).toBe(expected)
   })
 
   it('should render a full width button', () => {
     const expected = 'w-full'
-    const wrapper = mount(<Button block />)
+    const wrapper = mount(<Button aria-label="test" block />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
 
   it('should render a regular button', () => {
     const expected = 'px-4 py-2 rounded-lg text-sm'
-    const wrapper = mount(<Button />)
+    const wrapper = mount(<Button aria-label="test" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
 
   it('should extend classes', () => {
     const expected = 'ml-2'
-    const wrapper = mount(<Button className="ml-2" />)
+    const wrapper = mount(<Button aria-label="test" className="ml-2" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
@@ -101,14 +101,14 @@ describe('Base Button', () => {
 describe('Sizes', () => {
   it('should render a larger button', () => {
     const expected = 'px-10 py-4 rounded-lg'
-    const wrapper = mount(<Button size="larger" />)
+    const wrapper = mount(<Button aria-label="test" size="larger" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
 
   it('should render a large button', () => {
     const expected = 'px-5 py-3 rounded-lg'
-    const wrapper = mount(<Button size="large" />)
+    const wrapper = mount(<Button aria-label="test" size="large" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
@@ -116,14 +116,14 @@ describe('Sizes', () => {
   // the default case is tested in the Base Button suite
   it('should render a regular button', () => {
     const expected = 'px-4 py-2 rounded-lg text-sm'
-    const wrapper = mount(<Button size="regular" />)
+    const wrapper = mount(<Button aria-label="test" size="regular" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
 
   it('should render a small button', () => {
     const expected = 'px-3 py-1 rounded-md text-sm'
-    const wrapper = mount(<Button size="small" />)
+    const wrapper = mount(<Button aria-label="test" size="small" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
@@ -132,28 +132,28 @@ describe('Sizes', () => {
 describe('Primary Button', () => {
   it('should contain primary base classes', () => {
     const expected = 'text-white bg-purple-600 border border-transparent'
-    const wrapper = mount(<Button />)
+    const wrapper = mount(<Button aria-label="test" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
 
   it('should contain primary active classes', () => {
     const expected = 'active:bg-purple-600 hover:bg-purple-700 focus:shadow-outline-purple'
-    const wrapper = mount(<Button />)
+    const wrapper = mount(<Button aria-label="test" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
 
   it('should contain primary disabled classes', () => {
     const expected = 'opacity-50 cursor-not-allowed'
-    const wrapper = mount(<Button disabled />)
+    const wrapper = mount(<Button aria-label="test" disabled />)
 
     expect(wrapper.find('button[disabled]').getDOMNode().getAttribute('class')).toContain(expected)
   })
 
   it('should not contain primary active classes when disabled', () => {
     const expected = 'active:bg-purple-600 hover:bg-purple-700 focus:shadow-outline-purple'
-    const wrapper = mount(<Button disabled />)
+    const wrapper = mount(<Button aria-label="test" disabled />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).not.toContain(expected)
   })
@@ -162,7 +162,7 @@ describe('Primary Button', () => {
 describe('Outline Button', () => {
   it('should contain outline base classes', () => {
     const expected = 'text-gray-600 border-gray-300 border dark:text-gray-400 focus:outline-none'
-    const wrapper = mount(<Button layout="outline" />)
+    const wrapper = mount(<Button aria-label="test" layout="outline" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
@@ -170,14 +170,14 @@ describe('Outline Button', () => {
   it('should contain outline active classes', () => {
     const expected =
       'active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:shadow-outline-gray'
-    const wrapper = mount(<Button layout="outline" />)
+    const wrapper = mount(<Button aria-label="test" layout="outline" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
 
   it('should contain outline disabled classes', () => {
     const expected = 'opacity-50 cursor-not-allowed bg-gray-300'
-    const wrapper = mount(<Button layout="outline" disabled />)
+    const wrapper = mount(<Button aria-label="test" layout="outline" disabled />)
 
     expect(wrapper.find('button[disabled]').getDOMNode().getAttribute('class')).toContain(expected)
   })
@@ -186,7 +186,7 @@ describe('Outline Button', () => {
 describe('Link Button', () => {
   it('should contain link base classes', () => {
     const expected = 'text-gray-600 dark:text-gray-400 focus:outline-none'
-    const wrapper = mount(<Button layout="link" />)
+    const wrapper = mount(<Button aria-label="test" layout="link" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
@@ -194,14 +194,14 @@ describe('Link Button', () => {
   it('should contain link active classes', () => {
     const expected =
       'active:bg-transparent hover:bg-gray-100 focus:shadow-outline-gray dark:hover:bg-gray-500 dark:hover:text-gray-300 dark:hover:bg-opacity-10'
-    const wrapper = mount(<Button layout="link" />)
+    const wrapper = mount(<Button aria-label="test" layout="link" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
 
   it('should contain link disabled classes', () => {
     const expected = 'opacity-50 cursor-not-allowed'
-    const wrapper = mount(<Button layout="link" disabled />)
+    const wrapper = mount(<Button aria-label="test" layout="link" disabled />)
 
     expect(wrapper.find('button[disabled]').getDOMNode().getAttribute('class')).toContain(expected)
   })
@@ -211,7 +211,7 @@ describe('DropdownItem Button', () => {
   it('should contain DropdowItem button classes', () => {
     const expected =
       'inline-flex items-center cursor-pointer w-full px-2 py-1 text-sm font-medium transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200'
-    const wrapper = mount(<Button layout="__dropdownItem" />)
+    const wrapper = mount(<Button aria-label="test" layout="__dropdownItem" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
@@ -220,7 +220,7 @@ describe('DropdownItem Button', () => {
 describe('Pagination Button', () => {
   it('should contain PageButton classes', () => {
     const expected = 'px-3 py-1 rounded-md text-xs'
-    const wrapper = mount(<Button size="pagination" />)
+    const wrapper = mount(<Button aria-label="test" size="pagination" />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expected)
   })
@@ -229,7 +229,7 @@ describe('Pagination Button', () => {
 describe('Icon', () => {
   it('should contain an svg as a children', () => {
     const wrapper = mount(
-      <Button>
+      <Button aria-label="test">
         <HeartIcon />
       </Button>
     )
@@ -269,7 +269,7 @@ describe('Icon', () => {
 
   it('should not contain left or right styles', () => {
     const expected = 'mr-2 -ml-1 ml-2 -mr-1'
-    const wrapper = mount(<Button icon={HeartIcon} />)
+    const wrapper = mount(<Button aria-label="test" icon={HeartIcon} />)
 
     expect(wrapper.find('svg').getDOMNode().getAttribute('class')).not.toContain(expected)
   })
@@ -298,7 +298,7 @@ describe('Icon', () => {
   it('should contain regular sized button icon classes', () => {
     const expectedButton = 'p-2 rounded-lg'
     const expectedSvg = 'h-5 w-5'
-    const wrapper = mount(<Button icon={HeartIcon} />)
+    const wrapper = mount(<Button aria-label="test" icon={HeartIcon} />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expectedButton)
     expect(wrapper.find('svg').getDOMNode().getAttribute('class')).toContain(expectedSvg)
@@ -307,7 +307,7 @@ describe('Icon', () => {
   it('should contain small sized button icon classes', () => {
     const expectedButton = 'p-2 rounded-md'
     const expectedSvg = 'h-3 w-3'
-    const wrapper = mount(<Button size="small" icon={HeartIcon} />)
+    const wrapper = mount(<Button aria-label="test" size="small" icon={HeartIcon} />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expectedButton)
     expect(wrapper.find('svg').getDOMNode().getAttribute('class')).toContain(expectedSvg)
@@ -316,7 +316,7 @@ describe('Icon', () => {
   it('should contain large sized button icon classes', () => {
     const expectedButton = 'p-3 rounded-lg'
     const expectedSvg = 'h-5 w-5'
-    const wrapper = mount(<Button size="large" icon={HeartIcon} />)
+    const wrapper = mount(<Button aria-label="test" size="large" icon={HeartIcon} />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expectedButton)
     expect(wrapper.find('svg').getDOMNode().getAttribute('class')).toContain(expectedSvg)
@@ -325,7 +325,7 @@ describe('Icon', () => {
   it('should contain larger sized button icon classes', () => {
     const expectedButton = 'p-4 rounded-lg'
     const expectedSvg = 'h-5 w-5'
-    const wrapper = mount(<Button size="larger" icon={HeartIcon} />)
+    const wrapper = mount(<Button aria-label="test" size="larger" icon={HeartIcon} />)
 
     expect(wrapper.find('button').getDOMNode().getAttribute('class')).toContain(expectedButton)
     expect(wrapper.find('svg').getDOMNode().getAttribute('class')).toContain(expectedSvg)
