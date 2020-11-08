b0855c7152f71618c548876a64ccc5573e65c509
Estevan Maito <ejmaito@gmail.com>
2020-11-08 15:23:45 -0300

    feat(alert): add alert component

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -1,4 +1,22 @@
 export default {
+  // Alert
+  alert: {
+    base: 'p-4 pl-12 relative rounded-lg leading-5',
+    withClose: 'pr-12',
+    success: 'bg-green-50 text-green-900 dark:bg-green-600 dark:text-white',
+    danger: 'bg-red-50 text-red-900 dark:bg-red-600 dark:text-white',
+    warning: 'bg-yellow-50 text-yellow-900 dark:bg-yellow-600 dark:text-white',
+    neutral: 'bg-gray-50 text-gray-800 dark:bg-gray-700 dark:text-gray-300',
+    info: 'bg-blue-50 text-blue-900 dark:bg-blue-600 dark:text-white',
+    icon: {
+      base: 'h-5 w-5',
+      success: 'text-green-400 dark:text-green-300',
+      danger: 'text-red-400 dark:text-red-300',
+      warning: 'text-yellow-400 dark:text-yellow-100',
+      neutral: 'text-gray-400 dark:text-gray-500',
+      info: 'text-blue-400 dark:text-blue-300',
+    },
+  },
   // Pagination
   pagination: {
     base: 'flex flex-col justify-between text-xs sm:flex-row text-gray-600 dark:text-gray-400',
