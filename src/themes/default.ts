8ffe341f71c8e0cd6c1b11061a40a6067ecf67fd
Dan Habot <dan.habot@gmail.com>
2021-08-17 01:07:07 -0400

    Replaced colors with config colors

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -1,169 +1,122 @@
 export default {
-  // Alert
   alert: {
     base: 'p-4 pl-12 relative rounded-lg leading-5',
     withClose: 'pr-12',
-    success: 'bg-green-50 text-green-900 dark:bg-green-600 dark:text-white',
-    danger: 'bg-red-50 text-red-900 dark:bg-red-600 dark:text-white',
-    warning: 'bg-yellow-50 text-yellow-900 dark:bg-yellow-600 dark:text-white',
-    neutral: 'bg-gray-50 text-gray-800 dark:bg-gray-700 dark:text-gray-300',
-    info: 'bg-blue-50 text-blue-900 dark:bg-blue-600 dark:text-white',
+    success: 'bg-success-50 text-success-900 dark:bg-success-600 dark:text-darkText',
+    danger: 'bg-error-50 text-error-900 dark:bg-error-600 dark:text-darkText',
+    warning: 'bg-warning-50 text-warning-900 dark:bg-warning-600 dark:text-darkText',
+    neutral: 'bg-neutral-50 text-neutral-800 dark:bg-neutral-700 dark:text-neutral-300',
+    info: 'bg-info-50 text-info-900 dark:bg-info-600 dark:text-darkText',
     icon: {
       base: 'h-5 w-5',
-      success: 'text-green-400 dark:text-green-300',
-      danger: 'text-red-400 dark:text-red-300',
-      warning: 'text-yellow-400 dark:text-yellow-100',
-      neutral: 'text-gray-400 dark:text-gray-500',
-      info: 'text-blue-400 dark:text-blue-300',
+      success: 'text-success-400 dark:text-success-300',
+      danger: 'text-error-400 dark:text-error-300',
+      warning: 'text-warning-400 dark:text-warning-100',
+      neutral: 'text-neutral-400 dark:text-neutral-500',
+      info: 'text-info-400 dark:text-info-300',
     },
   },
-  // Pagination
   pagination: {
-    base: 'flex flex-col justify-between text-xs sm:flex-row text-gray-600 dark:text-gray-400',
+    base:
+      'flex flex-col justify-between text-xs sm:flex-row text-neutral-600 dark:text-neutral-400',
   },
-  // TableFooter
   tableFooter: {
     base:
-      'px-4 py-3 border-t dark:border-gray-700 bg-gray-50 text-gray-500 dark:text-gray-400 dark:bg-gray-800',
+      'px-4 py-3 border-t dark:border-neutral-700 bg-neutral-50 text-neutral-500 dark:text-neutral-400 dark:bg-neutral-800',
   },
-  // TableRow
-  tableRow: {
-    base: '',
-  },
-  // TableHeader
+  tableRow: { base: '' },
   tableHeader: {
     base:
-      'text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800',
+      'text-xs font-semibold tracking-wide text-left text-neutral-500 uppercase border-b dark:border-neutral-700 bg-neutral-50 dark:text-neutral-400 dark:bg-neutral-800',
   },
-  // TableContainer
-  tableContainer: {
-    base: 'w-full overflow-hidden rounded-lg ring-1 ring-black ring-opacity-5',
-  },
-  // TableCell
-  tableCell: {
-    base: 'px-4 py-3',
-  },
-  // TableBody
+  tableContainer: { base: 'w-full overflow-hidden rounded-lg ring-1 ring-black ring-opacity-5' },
+  tableCell: { base: 'px-4 py-3' },
   tableBody: {
     base:
-      'bg-white divide-y dark:divide-gray-700 dark:bg-gray-800 text-gray-700 dark:text-gray-400',
+      'bg-darkText divide-y dark:divide-neutral-700 dark:bg-neutral-800 text-neutral-700 dark:text-neutral-400',
   },
-  // DropdownItem
-  // this is the <li> that lives inside the Dropdown <ul>
-  // you're probably looking for the dropdownItem style inside button
-  dropdownItem: {
-    base: 'mb-2 last:mb-0',
-  },
-  // Dropdown
+  dropdownItem: { base: 'mb-2 last:mb-0' },
   dropdown: {
     base:
-      'absolute w-56 p-2 mt-2 text-gray-600 bg-white border border-gray-100 rounded-lg shadow-md min-w-max-content dark:text-gray-300 dark:border-gray-700 dark:bg-gray-700',
-    align: {
-      left: 'left-0',
-      right: 'right-0',
-    },
+      'absolute w-56 p-2 mt-2 text-neutral-600 bg-darkText border border-neutral-100 rounded-lg shadow-md min-w-max-content dark:text-neutral-300 dark:border-neutral-700 dark:bg-neutral-700',
+    align: { left: 'left-0', right: 'right-0' },
   },
-  // Avatar
   avatar: {
     base: 'relative rounded-full inline-block',
-    size: {
-      large: 'w-10 h-10',
-      regular: 'w-8 h-8',
-      small: 'w-6 h-6',
-    },
+    size: { large: 'w-10 h-10', regular: 'w-8 h-8', small: 'w-6 h-6' },
   },
-  // Modal
   modal: {
     base:
-      'w-full px-6 py-4 overflow-hidden bg-white rounded-t-lg dark:bg-gray-800 sm:rounded-lg sm:m-4 sm:max-w-xl',
+      'w-full px-6 py-4 overflow-hidden bg-darkText rounded-t-lg dark:bg-neutral-800 sm:rounded-lg sm:m-4 sm:max-w-xl',
   },
-  // ModalBody
-  modalBody: {
-    base: 'mb-6 text-sm text-gray-700 dark:text-gray-400',
-  },
-  // ModalFooter
+  modalBody: { base: 'mb-6 text-sm text-neutral-700 dark:text-neutral-400' },
   modalFooter: {
     base:
-      'flex flex-col items-center justify-end px-6 py-3 -mx-6 -mb-4 space-y-4 sm:space-y-0 sm:space-x-6 sm:flex-row bg-gray-50 dark:bg-gray-800',
+      'flex flex-col items-center justify-end px-6 py-3 -mx-6 -mb-4 space-y-4 sm:space-y-0 sm:space-x-6 sm:flex-row bg-neutral-50 dark:bg-neutral-800',
   },
-  // ModalHeader
-  modalHeader: {
-    base: 'mt-4 mb-2 text-lg font-semibold text-gray-700 dark:text-gray-300',
-  },
-  // Badge
+  modalHeader: { base: 'mt-4 mb-2 text-lg font-semibold text-neutral-700 dark:text-neutral-300' },
   badge: {
     base: 'inline-flex px-2 text-xs font-medium leading-5 rounded-full',
-    success: 'text-green-700 bg-green-100 dark:bg-green-700 dark:text-green-100',
-    danger: 'text-red-700 bg-red-100 dark:text-red-100 dark:bg-red-700',
-    warning: 'text-orange-700 bg-orange-100 dark:text-white dark:bg-orange-600',
-    neutral: 'text-gray-700 bg-gray-100 dark:text-gray-100 dark:bg-gray-700',
-    primary: 'text-purple-700 bg-purple-100 dark:text-white dark:bg-purple-600',
+    success: 'text-success-700 bg-success-100 dark:bg-success-700 dark:text-success-100',
+    danger: 'text-error-700 bg-error-100 dark:text-error-100 dark:bg-error-700',
+    warning: 'text-darkWarning-700 bg-darkWarning-100 dark:text-darkText dark:bg-darkWarning-600',
+    neutral: 'text-neutral-700 bg-neutral-100 dark:text-neutral-100 dark:bg-neutral-700',
+    primary: 'text-primary-700 bg-primary-100 dark:text-darkText dark:bg-primary-600',
   },
-  // Backdrop
   backdrop: {
     base:
       'fixed inset-0 z-40 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center',
   },
-  // Textarea
   textarea: {
-    base: 'block w-full text-sm dark:text-gray-300 rounded-md focus:outline-none',
+    base: 'block w-full text-sm dark:text-neutral-300 rounded-md focus:outline-none',
     active:
-      'focus:border-purple-400 border-gray-300 dark:border-gray-600 dark:focus:border-gray-600 dark:bg-gray-700 dark:focus:ring-gray-300 focus:ring focus:ring-purple-300',
-    disabled: 'cursor-not-allowed opacity-50 bg-gray-300 dark:bg-gray-800',
+      'focus:border-primary-400 border-neutral-300 dark:border-neutral-600 dark:focus:border-neutral-600 dark:bg-neutral-700 dark:focus:ring-neutral-300 focus:ring focus:ring-primary-300',
+    disabled: 'cursor-not-allowed opacity-50 bg-neutral-300 dark:bg-neutral-800',
     valid:
-      'border-green-600 dark:bg-gray-700 focus:border-green-400 dark:focus:border-green-400 focus:ring focus:ring-green-200 dark:focus:ring-green-200',
+      'border-success-600 dark:bg-neutral-700 focus:border-success-400 dark:focus:border-success-400 focus:ring focus:ring-success-200 dark:focus:ring-success-200',
     invalid:
-      'border-red-600 dark:bg-gray-700 focus:border-red-400 dark:focus:border-red-400 focus:ring focus:ring-red-200 dark:focus:ring-red-200',
+      'border-error-600 dark:bg-neutral-700 focus:border-error-400 dark:focus:border-error-400 focus:ring focus:ring-error-200 dark:focus:ring-error-200',
   },
-  // Select
   select: {
-    base: 'block w-full text-sm dark:text-gray-300 focus:outline-none rounded-md',
+    base: 'block w-full text-sm dark:text-neutral-300 focus:outline-none rounded-md',
     active:
-      'focus:border-purple-400 border-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:ring focus:ring-purple-300 dark:focus:ring-gray-300 dark:focus:border-gray-600',
+      'focus:border-primary-400 border-neutral-300 dark:border-neutral-600 dark:bg-neutral-700 focus:ring focus:ring-primary-300 dark:focus:ring-neutral-300 dark:focus:border-neutral-600',
     select: 'leading-5',
-    disabled: 'cursor-not-allowed opacity-50 bg-gray-300 dark:bg-gray-800',
+    disabled: 'cursor-not-allowed opacity-50 bg-neutral-300 dark:bg-neutral-800',
     valid:
-      'border-green-600 dark:bg-gray-700 focus:border-green-400 dark:focus:border-green-400 focus:ring focus:ring-green-200 dark:focus:ring-green-200',
+      'border-success-600 dark:bg-neutral-700 focus:border-success-400 dark:focus:border-success-400 focus:ring focus:ring-success-200 dark:focus:ring-success-200',
     invalid:
-      'border-red-600 dark:bg-gray-700 focus:border-red-400 dark:focus:border-red-400 focus:ring focus:ring-red-200 dark:focus:ring-red-200',
+      'border-error-600 dark:bg-neutral-700 focus:border-error-400 dark:focus:border-error-400 focus:ring focus:ring-error-200 dark:focus:ring-error-200',
   },
-  // Label
   label: {
-    base: 'block text-sm text-gray-700 dark:text-gray-400',
-    // check and radio get this same style
+    base: 'block text-sm text-neutral-700 dark:text-neutral-400',
     check: 'inline-flex items-center',
     disabled: 'opacity-50 cursor-not-allowed',
   },
-  // Input
   input: {
-    base: 'block w-full text-sm focus:outline-none dark:text-gray-300 leading-5 rounded-md',
+    base: 'block w-full text-sm focus:outline-none dark:text-neutral-300 leading-5 rounded-md',
     active:
-      'focus:border-purple-400 border-gray-300 dark:border-gray-600 focus:ring focus:ring-purple-300 dark:focus:border-gray-600 dark:focus:ring-gray-300 dark:bg-gray-700',
-    disabled: 'cursor-not-allowed opacity-50 bg-gray-300 dark:bg-gray-800',
+      'focus:border-primary-400 border-neutral-300 dark:border-neutral-600 focus:ring focus:ring-primary-300 dark:focus:border-neutral-600 dark:focus:ring-neutral-300 dark:bg-neutral-700',
+    disabled: 'cursor-not-allowed opacity-50 bg-neutral-300 dark:bg-neutral-800',
     valid:
-      'border-green-600 dark:bg-gray-700 focus:border-green-400 dark:focus:border-green-400 focus:ring focus:ring-green-200 dark:focus:ring-green-200',
+      'border-success-600 dark:bg-neutral-700 focus:border-success-400 dark:focus:border-success-400 focus:ring focus:ring-success-200 dark:focus:ring-success-200',
     invalid:
-      'border-red-600 dark:bg-gray-700 focus:border-red-400 dark:focus:border-red-400 focus:ring focus:ring-red-200 dark:focus:ring-red-200',
+      'border-error-600 dark:bg-neutral-700 focus:border-error-400 dark:focus:border-error-400 focus:ring focus:ring-error-200 dark:focus:ring-error-200',
     radio:
-      'text-purple-600 form-radio focus:border-purple-400 focus:outline-none focus:ring focus:ring-purple-300 focus:ring-offset-0 dark:focus:ring-gray-300',
+      'text-primary-600 form-radio focus:border-primary-400 focus:outline-none focus:ring focus:ring-primary-300 focus:ring-offset-0 dark:focus:ring-neutral-300',
     checkbox:
-      'text-purple-600 form-checkbox focus:border-purple-400 focus:outline-none focus:ring focus:ring-purple-300 focus:ring-offset-0 rounded dark:focus:ring-gray-300',
+      'text-primary-600 form-checkbox focus:border-primary-400 focus:outline-none focus:ring focus:ring-primary-300 focus:ring-offset-0 rounded dark:focus:ring-neutral-300',
   },
-  // HelperText
   helperText: {
     base: 'text-xs',
-    valid: 'text-green-600 dark:text-green-400',
-    invalid: 'text-red-600 dark:text-red-400',
+    valid: 'text-success-600 dark:text-success-400',
+    invalid: 'text-error-600 dark:text-error-400',
   },
-  // Card
   card: {
     base: 'min-w-0 rounded-lg ring-1 ring-black ring-opacity-5 overflow-hidden',
-    default: 'bg-white dark:bg-gray-800',
+    default: 'bg-darkText dark:bg-neutral-800',
   },
-  cardBody: {
-    base: 'p-4',
-  },
-  // Button
+  cardBody: { base: 'p-4' },
   button: {
     base:
       'align-bottom inline-flex items-center justify-center cursor-pointer leading-5 transition-colors duration-150 font-medium focus:outline-none',
@@ -181,7 +134,6 @@ export default {
       },
       pagination: 'px-3 py-1 rounded-md text-xs',
     },
-    // styles applied to the SVG icon
     icon: {
       larger: 'h-5 w-5',
       large: 'h-5 w-5',
@@ -191,26 +143,25 @@ export default {
       right: 'ml-2 -mr-1',
     },
     primary: {
-      base: 'text-white bg-purple-600 border border-transparent',
-      active: 'active:bg-purple-600 hover:bg-purple-700 focus:ring focus:ring-purple-300',
+      base: 'text-darkText bg-primary-600 border border-transparent',
+      active: 'active:bg-primary-600 hover:bg-primary-700 focus:ring focus:ring-primary-300',
       disabled: 'opacity-50 cursor-not-allowed',
     },
     outline: {
-      base: 'text-gray-600 border-gray-300 border dark:text-gray-400 focus:outline-none',
+      base: 'text-neutral-600 border-neutral-300 border dark:text-neutral-400 focus:outline-none',
       active:
-        'active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:ring focus:ring-gray-300',
-      disabled: 'opacity-50 cursor-not-allowed bg-gray-300',
+        'active:bg-transparent hover:border-neutral-500 focus:border-neutral-500 active:text-neutral-500 focus:ring focus:ring-neutral-300',
+      disabled: 'opacity-50 cursor-not-allowed bg-neutral-300',
     },
     link: {
-      base: 'text-gray-600 dark:text-gray-400 focus:outline-none border border-transparent',
+      base: 'text-neutral-600 dark:text-neutral-400 focus:outline-none border border-transparent',
       active:
-        'active:bg-transparent hover:bg-gray-100 focus:ring focus:ring-gray-300 dark:hover:bg-gray-500 dark:hover:text-gray-300 dark:hover:bg-opacity-10',
+        'active:bg-transparent hover:bg-neutral-100 focus:ring focus:ring-neutral-300 dark:hover:bg-neutral-500 dark:hover:text-neutral-300 dark:hover:bg-opacity-10',
       disabled: 'opacity-50 cursor-not-allowed',
     },
-    // this is the button that lives inside the DropdownItem
     dropdownItem: {
       base:
-        'inline-flex items-center cursor-pointer w-full px-2 py-1 text-sm font-medium transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200',
+        'inline-flex items-center cursor-pointer w-full px-2 py-1 text-sm font-medium transition-colors duration-150 rounded-md hover:bg-neutral-100 hover:text-neutral-800 dark:hover:bg-neutral-800 dark:hover:text-neutral-200',
     },
   },
 }
