ce734c59e956325dfdb127422f13827a86bff3b4
Asim-Tahir <AsimTahir@protonmail.com>
2021-09-13 17:48:48 +0300

    chore(sb): version bump and config update

--- .storybook/main.js
+++ .storybook/main.js
@@ -20,5 +20,18 @@ module.exports = {
     return config
   },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    "@storybook/addon-actions",
+    "@storybook/addon-controls",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
 }
