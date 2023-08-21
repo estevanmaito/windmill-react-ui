e51b22dddddd1006979b1ddc46ca73d6165cd6bc
sandikodev <androxoss@hotmail.com>
2023-08-22 00:45:55 +0700

    chore(deps): updated to jest-29

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+const { defaults: tsjPreset } = require('ts-jest/presets')
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>setupTests.ts'],
+  transform: {
+    ...tsjPreset.transform,
+    // "^.+\\.svg$": "jest-svg-transformer",
+  },
+  testPathIgnorePatterns: ['.*\\.d\\.ts', '/node_modules/'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/{Transition,index}.{ts,tsx}',
+    '!src/{stories,utils}/*',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+}
